5e998ea13a6a6cc083c1b19719e032a9f3195b53
Sean Dawson <spdawson@agiledigital.com.au>
2020-04-29 09:02:11 +1000

    build(deps): bump reagent to 1.0.0-alpha1
    - This brings in the Firefox extension bug fix I wrote so the fork is no longer needed

--- src/main/mule_preview/dev/init.cljs
+++ src/main/mule_preview/dev/init.cljs
@@ -1,7 +1,7 @@
 (ns mule-preview.dev.init
   "Functions only used by dev tools when running locally"
   (:require
-   [reagent.core :as r]
+   [reagent.dom :as rd]
    [mule-preview.client.core :refer [mule-preview-diff-url]]))
 
 (defn ^:dev/after-load init! []
@@ -9,4 +9,4 @@
                    {:content-urls ["/example_xml/nice-example.xml"
                                    "/example_xml/nice-example-diff.xml"] 
                     :content-root "."})]
-     (r/render [component] (.getElementById js/document "app"))))
+     (rd/render [component] (.getElementById js/document "app"))))
